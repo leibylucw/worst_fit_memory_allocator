b496bddb0b73741617658a9c4d00123612d92e33
Luke Leiby <lucaswleiby@gmail.com>
2022-04-22 19:00:57 -0400

    Implement everything except splitting and coalescing

--- src/mymalloc.c
+++ src/mymalloc.c
@@ -13,11 +13,73 @@
 #define SIZE_MULTIPLE       8
 
 typedef struct Header {
-	int data_size;
+	unsigned int data_size;
 	bool used;
 	struct Header* previous;
 	struct Header* next;
 } Header;
+
+Header* head;
+Header* tail;
+
+bool is_head(Header* ptr) {
+	return ptr == head;
+}
+
+bool is_tail(Header* ptr) {
+	return ptr == tail;
+}
+
+void insert_header(Header* ptr) {
+	if (ptr == NULL) {
+		return;
+	}
+
+	else if (head == NULL) {
+		head = ptr;
+		tail = ptr;
+	}
+
+	else {
+		tail->next = ptr;
+		ptr->previous = tail;
+		tail = ptr;
+	}
+}
+
+Header* construct_new_header(unsigned int size) {
+	Header* h = sbrk(sizeof(Header) + size);
+	h->data_size = size;
+	h->used = true;
+	h->previous = NULL;
+	h->next = NULL;
+
+	return h;
+}
+
+Header* get_chunk_header(void* ptr) {
+	return PTR_ADD_BYTES(ptr, sizeof(Header)*-1);
+}
+
+Header* get_worst_fit(unsigned int size) {
+	Header* largest = NULL;
+	Header* cur;
+
+	for (cur = head; cur != NULL; cur = cur->next) {
+		if (cur->data_size > size && cur->used == false) {
+			largest = cur;
+		}
+	}
+
+	return largest;
+}
+
+void print_headers() {
+	Header* cur;
+	for (cur = head; cur != NULL; cur = cur->next) {
+		printf("%d, %d\n", cur->data_size, cur->used);
+	}
+}
 
 unsigned int round_up_size(unsigned int data_size) {
 	if(data_size == 0)
@@ -34,25 +96,32 @@ void* my_malloc(unsigned int size) {
 
 	size = round_up_size(size);
 
-	// ------- Don't remove anything above this line. -------
-	// Here's where you'll put your code!
+	Header* largest = get_worst_fit(size);
 
-	// AT LEAST SOME OF IT
+if (largest == NULL) {
+	Header* h = construct_new_header(size);
+	insert_header(h);
+	return  PTR_ADD_BYTES(tail, sizeof(Header));
+}
 
-	// USE FUNCTIONS
-
-	// PLEASE
-
-	void* newBreak = sbrk(size);
-
-	return newBreak;
+	else {
+		largest->used = true;
+		return  PTR_ADD_BYTES(largest, sizeof(Header));
+	}
 }
 
 void my_free(void* ptr) {
 	if(ptr == NULL)
 		return;
 
-	// and here's where you free stuff.
+	Header* chunk_header = get_chunk_header(ptr);
 
-	brk(ptr);
+	if (is_tail(chunk_header)) {
+		tail = chunk_header->previous;
+		brk(chunk_header);
+	}
+
+	else {
+		chunk_header->used = false;
+	}
 }
