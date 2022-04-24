c3ad4029bd3fdf5fe2058274e7c9629ee8e3a11c
Luke Leiby <lucaswleiby@gmail.com>
2022-04-23 20:32:54 -0400

    Add personal tests

--- src/mydriver.c
+++ src/mydriver.c
@@ -3,6 +3,72 @@
 
 #include "mymalloc.h"
 
+void test_worst_fit() {
+	printf("Testing worst fit algorithm...\n");
+
+	void* a = my_malloc(20);
+	void* b = my_malloc(30);
+	void* c = my_malloc(40);
+
+	my_free(b);
+
+	void* d = my_malloc(30);
+
+	if (d != b) {
+		printf("d should point to the same block as b\n");
+	} else {
+		printf("All good!\n");
+	}
+
+	my_free(a);
+	my_free(c);
+}
+
+void test_coalesce() {
+	printf("Testing coalescing algorithm...\n");
+
+	void* a = my_malloc(16);
+	void* b = my_malloc(16);
+	void* c = my_malloc(50);
+
+	my_free(a);
+	my_free(b);
+
+	void* d = my_malloc(20);
+
+	if (a != d) {
+		printf("a should point to the same block as d\n");
+	} else {
+		printf("All good!\n");
+	}
+
+	my_free(c);
+}
+
+void test_split() {
+	printf("Testing splitting algorithm...\n");
+
+	void* a = my_malloc(16);
+	void* b = my_malloc(64);
+	void* c = my_malloc(16);
+
+	my_free(b);
+
+	void* d = my_malloc(24);
+	void* e = my_malloc(16);
+
+	if (d == e) {
+		printf("d should not reference the same block as e\n");
+	} else {
+		printf("All good!\n");
+	}
+
+	my_free(a);
+	my_free(c);
+	my_free(d);
+	my_free(e);
+}
+
 int main() {
 	// You can use sbrk(0) to get the current position of the break.
 	// This is nice for testing cause you can see if the heap is the same size
@@ -19,6 +85,10 @@ int main() {
 		printf("Hmm, the heap got bigger by %u (0x%X) bytes...\n", heap_size_diff, heap_size_diff);
 
 	// ADD MORE TESTS HERE.
+
+	test_worst_fit();
+	test_coalesce();
+	test_split();
 
 	return 0;
 }
